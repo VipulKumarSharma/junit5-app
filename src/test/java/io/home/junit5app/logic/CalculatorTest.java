6445b1e69b93a76b8f0e2e9ebe4f552a90d3b22a
Vipul Kumar Sharma <tysonvks@gmail.com>
2021-02-28 20:05:00 +0530

    Grouping Test cases using @Nested class

--- src/test/java/io/home/junit5app/logic/CalculatorTest.java
+++ src/test/java/io/home/junit5app/logic/CalculatorTest.java
@@ -1,6 +1,7 @@
 package io.home.junit5app.logic;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,13 +19,6 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("Test addition of negative integers")
-    void negativeNumAddTest() {
-        RuntimeException exception = assertThrows(RuntimeException.class, () -> calculator.add(-5, 5));
-        assertEquals("Negative numbers are not allowed", exception.getMessage());
-    }
-
-    @Test
     @DisplayName("Test subtraction of positive integers")
     void subtract() {
         assertEquals(0, calculator.subtract(5, 5));
@@ -37,15 +31,26 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("Test division of with zero as denominator")
-    void divideByZeroTest() {
-        RuntimeException exception = assertThrows(RuntimeException.class, () -> calculator.divide(5, 0));
-        assertEquals("Zero is not allowed in denominator", exception.getMessage());
-    }
-
-    @Test
     @DisplayName("Test multiplication of positive integers")
     void multiply() {
         assertEquals(25, calculator.multiply(5, 5));
+    }
+
+    @Nested
+    @DisplayName("Exception cases")
+    class ExceptionCases {
+        @Test
+        @DisplayName("Test addition of negative integers")
+        void negativeNumAddTest() {
+            RuntimeException exception = assertThrows(RuntimeException.class, () -> calculator.add(-5, 5));
+            assertEquals("Negative numbers are not allowed", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("Test division of with zero as denominator")
+        void divideByZeroTest() {
+            RuntimeException exception = assertThrows(RuntimeException.class, () -> calculator.divide(5, 0));
+            assertEquals("Zero is not allowed in denominator", exception.getMessage());
+        }
     }
 }
