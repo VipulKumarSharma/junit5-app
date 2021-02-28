ca32fc966934842fd37bf574b4be8699f5a999cf
Vipul Kumar Sharma <tysonvks@gmail.com>
2021-02-28 15:10:23 +0530

    Added empty reactive project

---
+++ src/test/java/io/home/junit5app/Junit5AppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.home.junit5app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
